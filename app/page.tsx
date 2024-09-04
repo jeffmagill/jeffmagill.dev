3e984e882edcf9cd25b08955cf70478ae674255d
Andrew Magill <AndyMagill@Gmail.com>
2024-09-03 21:33:13 -0400

    added empty component skeleton and placeholder content

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,31 @@
 import Image from "next/image";
+import type { Metadata } from "next";
 import styles from "./page.module.css";
+
+export const metadata: Metadata = {
+    title: "Andrew Magill: Web Developer",
+    description: "Frontend Architect and Web Development Manager",
+};
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+        <div className={styles.wrapper}>
+            <h1>
+                <strong>Hey there, I’m Andrew&nbsp;Magill.</strong> I’m a 
+                senior web <a href="https://github.com/andymagill">developer</a> from 
+                New&nbsp;York with deep experience building custom websites and web 
+                applications. I’m currently looking for new challenges and
+                interesting opportunities. I built this page to showcase some 
+                of <a href="https://www.linkedin.com/in/andrew-magill">my&nbsp;work</a>.
+            </h1>
+
+            <Image src="/images/portrait-andrew-magill.jpg" 
+                alt="Andrew Magill: Web Developer" 
+                className={styles.portrait}
+                width={300} height={300} 
             />
-          </a>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   );
 }
---
+++ content/blog/hello-world.md
@@ -0,0 +1,21 @@
+---
+title: Hello World!
+descrition: The first of many blog posts. 
+image: 
+excerpt: I'll be honest, this is really just a test of my Next.JS application to successfully generate and publish a static website. 
+---
+
+# Hello World!
+Welcome to my first blog post! I'll be honest, this is really just a test of my Next.JS application to successfully generate and publish a static website.  If you are reading this, the test is a success. Hooray!
+
+## What are you talking about ?
+I created this blog to showcase my best work, discuss new technology trends, and share any insights I've learned along the way. I've avoided this kind of activity for too long and that attitude has caught up with me. I'll do my best to earn the time you spend here.
+
+For now, let's just test a code block : 
+```javascript
+function greet(name) {
+  console.log(`Hello, ${name}!`);
+}
+
+greet("World");
+```
