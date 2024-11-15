4770e692ab704857c5b475e20dba96b3b2837426
Andrew Magill <AndyMagill@Gmail.com>
2024-11-14 21:02:48 -0500

    removed icons and manifest from sitemap xml

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,4 +4,10 @@ module.exports = {
   siteUrl: process.env.SITE_URL || 'https://magill.dev',
   generateIndexSitemap: false,
   generateRobotsTxt: true,
+  exclude: [
+    '/apple-icon.png',
+    '/icon.png',
+    '/manifest.webmanifest',
+    '/feed/posts.json',
+  ],
 };
